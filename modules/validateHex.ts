c3ccdb6abf43ff2bee1ebc30e1777e131d291c04
vnnh <90565423+vnnh@users.noreply.github.com>
2022-07-27 21:07:46 -0500

    fix validateHex

--- modules/validateHex.ts
+++ modules/validateHex.ts
@@ -3,5 +3,5 @@ export default (hex: string | number) => {
 		hex = hex.toString();
 	}
 
-	return /(?:^#[0-9A-Fa-f]{8})|(?:^#[0-9A-Fa-f]{6})|(?:^#[0-9A-Fa-f]{3})/i.test(hex);
+	return /(?:[0-9A-Fa-f]{8})|(?:[0-9A-Fa-f]{6})|(?:[0-9A-Fa-f]{3})$/i.test(hex);
 };
